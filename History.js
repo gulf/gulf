3b8625f7855b6dd0a60c8a5dd9ce5273085514e9
Marcel Klehr <mklehr@gmx.net>
2013-09-03 22:51:17 +0200

    Implement History#prune() and History.findNewestCommonAncestor()

--- History.js
+++ History.js
@@ -37,3 +37,54 @@ History.prototype.getAllAfter = function(editId) {
   }
   return arr
 }
+
+/**
+ * Prunes an edit from a history of edits (supplied as a list of edit ids)
+ * 
+ * @return a chronological list of edit ojects
+ */
+History.prototype.prune = function(editId, history) {
+  if(!this.edits[editId]) throw new Error('Unknown edit')
+  
+  if(!~history.indexOf(editId)) return history // Nothing to prune
+  
+  var pruningEdit = this.edits[editId].clone().invert()
+  
+  var prunedHistory = history.slice(history.indexOf(editId)+1)
+      .map(function(otherEdit) {
+        otherEdit = this.edits[otherEdit].clone()
+        otherEdit.transformAgainst(pruningEdit)
+        
+        pruningEdit.transformAgainst(otherEdit)
+        return otherEdit
+      })
+  
+  return prunedHistory
+}
+
+/**
+ * Find the newest common ancestors of the passed edit and this document
+ */
+History.findNewestCommonAncestor = function(ancestors1, ancestors2) {
+  var anc1 = 0
+    , anc2 = 0
+    , commonAncestor
+  
+  if(~ancestors1.indexOf(ancestors2[0])) { // XXX Actually there's no need to call indexOf twice, it just looks more sane
+    anc1 = ancestors1.indexOf(ancestors2[0])
+  }
+  else if(~ancestors2.indexOf(ancestors1[0])) {
+    anc2 = ancestors2.indexOf(ancestors1[0])
+  }else{
+    throw new Error('Edit is of unknown descendancy')
+  }
+  
+  // find the newest common ancestor
+  while (ancestors1[anc1] == ancestors2[anc2]) {
+    commonAncestor = ancestors1[anc1]
+    anc1++
+    anc2++
+  }
+  
+  return ancestors1[anc1]
+}
